913226be3cc2198287c7099e477d89509b121019
EC2 Default User <junjun091028@gmail.com>
2025-03-08 07:19:08 +0000

    maps

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
       resources :post_comments, only: [:create, :destroy]
     end
     resources :users, only: [:show, :edit, :update]
+    resource :map, only: [:show]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_27_104428) do
+ActiveRecord::Schema.define(version: 2025_03_08_062832) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -73,6 +73,9 @@ ActiveRecord::Schema.define(version: 2024_11_27_104428) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address", default: "", null: false
+    t.float "latitude", default: 0.0, null: false
+    t.float "longitude", default: 0.0, null: false
   end
 
   create_table "users", force: :cascade do |t|
